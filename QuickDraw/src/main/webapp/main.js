929334f9c98cf64e4302d7fb6d2ad6ca8c82d038
William <lewivo@gmail.com>
2021-01-26 18:39:58 +0100

    added standard functions for progressbar and canvas

--- QuickDraw/src/main/webapp/main.js
+++ QuickDraw/src/main/webapp/main.js
@@ -5,3 +5,44 @@
  */
 
 
+var progressBarVal = 0;
+var progressBarDelay = 1000;
+function jump(page){
+  window.location.href = page;
+}
+function hideElem(tag){
+  $(tag).hide();
+}
+function showElem(tag){
+  $(tag).show();
+}
+function startProgressBar(){
+  setInterval(updateProgressBar, progressBarDelay);
+}
+function updateProgressBar(){
+  let progress= document.getElementById("p1");
+  var x = $("#p1").attr("aria-valuenow", progressBarVal); 
+  var x = $("#p1").css("width", progressBarVal + "%"); 
+  progressBarVal += 1;
+  console.log(x);
+}
+var canvas = document.getElementById("myCanvas");
+canvas.addEventListener("mousedown", function(e){
+  canvas.onmousemove= function(e){
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(e.x,e.y); 
+    setTimeout(function (){
+      ctx.lineTo(e.x,e.y);
+      ctx.stroke();
+    } , 1000);
+    
+  }
+});
+
+canvas.addEventListener("mouseup", function(e){
+  canvas.onmousemove=null;
+})
+
+//TODO implement a timer for the counter 20,19...  
+//TODO syncronize the progressbar with the counter
