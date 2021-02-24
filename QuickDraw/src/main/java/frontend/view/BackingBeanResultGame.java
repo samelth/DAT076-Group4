fc961df32f2ebdaabc661398ef8b706a7875517e
William <lewivo@gmail.com>
2021-02-24 06:22:28 +0100

    fixed duplicate naming error

--- QuickDraw/src/main/java/frontend/view/BackingBeanResultGame.java
+++ QuickDraw/src/main/java/frontend/view/BackingBeanResultGame.java
@@ -24,7 +24,7 @@ import javax.inject.Named;
  *
  * @author lewiv
  */
-@Named(value = "bbCreategame")
+@Named(value = "bbResultGame")
 @ViewScoped
 public class BackingBeanResultGame implements Serializable {
 	
