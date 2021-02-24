4e856c443a32a1b4561ec33f0e5d59971a27a04d
Karl Svensson <Svensson.Karl@iCloud.com>
2021-02-24 15:31:57 +0100

    Ability to retrieve drawing words from database

--- QuickDraw/src/main/java/frontend/view/BackingBeanDrawPage.java
+++ QuickDraw/src/main/java/frontend/view/BackingBeanDrawPage.java
@@ -17,8 +17,21 @@
 package frontend.view;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.Query;
+import model.database.dao.DrawingWordDAO;
+import model.database.dao.GameSessionDAO;
+import model.database.entity.DrawingWord;
+import model.database.entity.GameSession;
+import org.omnifaces.cdi.Param;
 
 /**
  *
@@ -27,5 +40,36 @@ import javax.inject.Named;
 @Named(value = "bbDrawPage")
 @ViewScoped
 public class BackingBeanDrawPage implements Serializable {
+  
+  @EJB
+  DrawingWordDAO drawingWordDAO;
+  
+  @EJB
+  GameSessionDAO gameSessionDAO;
+  
+  private LinkedList<DrawingWord> words;
+  private DrawingWord currentWord;
+  private GameSession currentGame;
+  
+  
+  private final int game_id = 1;
+  
+  @PostConstruct
+  public void init(){
+    currentGame = gameSessionDAO.find(game_id);
+    words = new LinkedList<>(drawingWordDAO.getWordsByLevel(currentGame.getLevel()));
+    Collections.shuffle(words);    
+  }
+  
+  public String getNextWord(){
+    
+    try{
+    currentWord = words.remove();
+    }catch(NullPointerException e){
+      System.err.println("Error: No more words to draw");
+    }
+    
+    return currentWord.getWord();
+  }
 
 }
